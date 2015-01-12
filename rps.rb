637f75e1e3bd301de268af9d6114175edccc00ce
guspowell <augustuspowell@me.com>
2015-01-12 11:32:19 +0000

    integrating sinatra

--- rps.rb
+++ rps.rb
@@ -1,17 +1,38 @@
 require 'sinatra/base'
 
+require_relative "lib/player.rb"
+require_relative "lib/options.rb"
+require_relative "lib/computer.rb"
+require_relative "lib/outcome.rb"
+
 class RPS < Sinatra::Base
+
+  enable :sessions
+
+  options = Options.new
 
   get '/' do
     @name= params[:name]
+    player = Player.new
+    session[:me] = player
+    player.name (@name)
     erb :index
   end
 
   get '/choice' do
-  	erb :choice
+    player = session[:me]
+    @choice = player.pick_option(:rock)
+    puts @choice
+    puts session.inspect
+    erb :choice
   end
 
   get '/outcome' do
+    outcome = Outcome.new
+    computer = Computer.new
+    @comp_choice = computer.comp_choice(options)
+    player = session[:me]
+    @outcome = outcome.outcome(player,computer)
     erb :outcome
   end
 
