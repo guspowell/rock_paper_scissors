97109321f624f39255b54b2e0c162f9df5a4c2fd
guspowell <augustuspowell@me.com>
2015-03-17 18:16:23 +0000

    results fade in one after the other

---
+++ public/javascripts/choices.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $('.player-choice').fadeIn(600, function(){
+    $('.computer-choice').fadeIn(600, function(){
+      $('.win-lose').fadeIn(600, function(){
+        $('#option.play-again').fadeIn(600).css('display','inline');
+      }).css('display','inline');
+    }).css('display','inline');
+  }).css('display','inline');
+
+});
